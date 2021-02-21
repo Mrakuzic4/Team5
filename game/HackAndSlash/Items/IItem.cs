486ee6c7c0f8bf70e1added12395f0c40f440095
Mrakuzic4 <78371194+Mrakuzic4@users.noreply.github.com>
2021-02-21 11:31:26 -0500

    Firewall Drawing fixed

--- game/HackAndSlash/Items/IItem.cs
+++ game/HackAndSlash/Items/IItem.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace HackAndSlash
 {
     interface IItem
     {
-        //void createItemAt();
+        void LoadContent();
 
-        //void changeToCollected(); // collect or equip
+        void Update();
 
-        //void useItem();
+        void Draw();
+
+        void CollectItem(IPlayer player);
+
+        void UseItem(int currentPlayerDirection, Vector2 currentPlayerPosition);
+
+        void ChangeToCollectable();
+
+        void ChangeToUseable();
+
+        void ChangeToBeingUsed();
+
+        void ChangeToExpended();
     }
 }
