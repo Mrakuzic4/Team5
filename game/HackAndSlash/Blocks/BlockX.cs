b402ef9d2065400409228f5e72725525525b907d
Daniel Neff <neff.331@buckeyemail.osu.edu>
2021-03-07 18:54:11 -0500

    small block changes

--- game/HackAndSlash/Blocks/BlockX.cs
+++ game/HackAndSlash/Blocks/BlockX.cs
@@ -13,12 +13,14 @@ namespace HackAndSlash
         private Texture2D block;
         private Vector2 location { get; set; }
         private SpriteBatch spriteBatch;
+        private Rectangle rectangle;
 
         public BlockX(Texture2D block, Vector2 location, SpriteBatch spriteBatch)
         {
             this.spriteBatch = spriteBatch;
             this.location = location;
             this.block = block;
+            rectangle = new Rectangle((int)location.X, (int)location.Y, GlobalSettings.BASE_SCALAR, GlobalSettings.BASE_SCALAR);
         }
 
         public void Update()
