0c8838406b1379a371097a06443a048a0e00393f
Amarth Gul <amarthgul@gmail.com>
2021-04-03 21:20:44 -0400

    Pushing the blcok now open the door

---
+++ game/HackAndSlash/Misc/LevelEagleSpecialCases.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackAndSlash
+{
+    /// <summary>
+    /// Dealing with the special cases in level eagle 
+    /// </summary>
+    class LevelEagleSpecialCases : ISpecialCases
+    {
+
+        public LevelEagleSpecialCases()
+        {
+
+        }
+
+        public void Update(Game1 game)
+        {
+
+            if (game.levelCycleRecord.currentLocationIndex.SequenceEqual(new int[] { 2, 1 }))
+            {
+                Room2_1(game);
+            }
+
+        }
+
+        // Open mys door with block move 
+        private void Room2_1(Game1 game)
+        {
+
+            foreach (IBlock block in game.blockList)
+            {
+                if (block is BlockMovable)
+                {
+                    BlockMovable BlockExaminer = (BlockMovable)block;
+                    if (BlockExaminer.Moved())
+                        game.currentLevel.OpenMysDoor((int)GlobalSettings.Direction.Left);
+                }
+            }
+
+        }
+    }
+}
+
+    
+
---
+++ game/HackAndSlash/Misc/SpecialCases.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackAndSlash
+{
+    public interface ISpecialCases
+    {
+
+        void Update(Game1 Game);
+    }
+}
