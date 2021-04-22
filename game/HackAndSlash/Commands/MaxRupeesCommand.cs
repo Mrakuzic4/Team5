285e558d003d5acda239a2c4f351ce8f4d2103ce
Daniel Neff <neff.331@buckeyemail.osu.edu>
2021-04-22 15:37:34 -0400

    Couple more cheats added

---
+++ game/HackAndSlash/Commands/MaxRupeesCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackAndSlash
+{
+    class MaxRupeesCommand : ICommand
+    {
+        Game1 Game;
+        public MaxRupeesCommand(Game1 game)
+        {
+            this.Game = game;
+        }
+
+        public void execute()
+        {
+            foreach (IItem item in Game.itemList)
+            {
+                if (item is RupyItem) item.SetMax();
+                Game.cheatText.activeText = CheatText.cheats.MAX_RUPEES;
+            }
+        }
+    }
+}
---
+++ game/HackAndSlash/Commands/NoFogCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackAndSlash
+{
+    class NoFogCommand : ICommand
+    {
+        Game1 Game;
+
+        public NoFogCommand(Game1 game)
+        {
+            this.Game = game;
+        }
+        public void execute()
+        {
+            Game._FOG = false;
+            Game.cheatText.activeText = CheatText.cheats.NO_FOG;
+        }
+    }
+}
