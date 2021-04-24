5a5791673a23804f202139b9441d7ef0a1a44c12
HKommaraju <48811258+HKommaraju@users.noreply.github.com>
2021-04-23 22:46:07 -0400

    Create DynamicGenerationsReview.cs

---
+++ PeerReviewS5/DynamicGenerationsReview.cs
@@ -0,0 +1,21 @@
+Author: Harshitha Kommaraju
+
+Date: Apr 22 2021
+
+Sprint Number: 5
+
+Name of the .cs file being reviewed: GenerateRoom.cs
+
+Author of the .cs file being reviewed: Amarth Chen
+
+Number of minutes taken to complete the review: 20
+
+For code's Readability: Variables and functions have clear names but are sometimes repetitive. Could have more comments explaining the functions and procedures through the file. 
+
+For codes' Maintainability: The code is highly maintainable since very few magic numbers are used and most of the looping is done dynamically.  
+
+A hypothetical change:
+
+There are a lot of functions in the class so a detailed workflow of what function calls what and how the class works would be helpful. 
+More commenting across the file to explain some really long arrays.
+Remove some variables and functions not used.
